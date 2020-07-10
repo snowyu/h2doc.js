ce6120195b3af605e59fe6cc57a64b7b11476212
Riceball LEE <snowyu.lee@gmail.com>
2020-07-10 18:43:47 +0800

    chore(build): update release-it script

--- .release-it.js
+++ .release-it.js
@@ -3,10 +3,18 @@ module.exports = {
     "publish": true
   },
   "git": {
+    "changelog": "npx auto-changelog --stdout --commit-limit false --unreleased --template config/changelog-compact.hbs",
     "commitMessage": "chore(build): release v${version}"
   },
   "github": {
     "release": true,
     "assets": ["dist/**/*.gz"]
+  },
+  "hooks": {
+    "before:init": ["npm test"],
+    "after:bump": [
+      "npx auto-changelog -p",
+      "npm run build"
+    ]
   }
 }
