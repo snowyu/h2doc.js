8d8ce3847b583d4fd4f1f800c6ee77eb85479968
Riceball LEE <snowyu.lee@gmail.com>
2020-07-10 18:39:42 +0800

    chore(test): update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
-const { off } = require("process");
+const off   = 0
+const warn  = 1
+const error = 2
 
 module.exports = {
   'env': {
@@ -7,22 +9,23 @@ module.exports = {
   'extends': [
     // 'plugin:@typescript-eslint/recommended',
     // 'google',
-    "oclif",
+    // "oclif",
     "oclif-typescript",
-    // 'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
   ],
-  // 'parser': '@typescript-eslint/parser',
-  // 'parserOptions': {
-  //   'ecmaVersion': 11,
-  //   'sourceType': 'module',
-  // },
-  // 'plugins': [
-  //   '@typescript-eslint',
-  // ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 11,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
   'rules': {
     'unicorn/no-abusive-eslint-disable': off,
     'no-template-curly-in-string': off,
     'no-multi-assign': off,
     '@typescript-eslint/no-use-before-define': off,
+    '@typescript-eslint/interface-name-prefix': off,
   },
 };
