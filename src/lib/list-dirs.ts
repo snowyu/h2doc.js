a8f9e5596664224dbe3d431b2c11f2d20999f923
Riceball LEE <snowyu.lee@gmail.com>
2020-07-11 11:21:14 +0800

    feat(command): list-dirs should not display the assets directory of the markdown file

--- src/lib/list-dirs.ts
+++ src/lib/list-dirs.ts
@@ -1,4 +1,5 @@
 import fg from 'fast-glob';
+import fs from 'fs';
 import path from 'path';
 import { getConfig } from './get-md-config';
 
@@ -11,11 +12,19 @@ export async function listDirs({
   exclude?: string | string[];
   deep?: number;
 }) {
-  root = path.join(root, '**');
+  // root = path.join(root, '**');
   if (!Array.isArray(exclude)) exclude = [exclude];
-  exclude = exclude.filter(Boolean).map(v => '!' + v);
-  exclude.unshift(root);
-  const result = (await fg(exclude, { onlyDirectories: true, deep })).sort();
+  // exclude = exclude.filter(Boolean).map(v => '!' + v);
+  // exclude.unshift(path.join(root, '**'));
+  const result = (
+    await fg(path.join(root, '**'), {
+      ignore: exclude,
+      onlyDirectories: true,
+      deep,
+    })
+  )
+    .filter(p => !(isFile(p + '.md') || isFile(p + '.markdown')))
+    .sort();
   return result;
 }
 
@@ -23,4 +32,18 @@ export async function getFolders() {
   const conf = await getConfig();
   const dirs = await listDirs(conf.output);
   return dirs;
+}
+
+export function isFile(filepath: string) {
+  let result: boolean;
+  try {
+    result = fs.lstatSync(filepath).isFile();
+  } catch (error) {
+    result = false;
+  }
+  return result;
+}
+
+export function relativePaths(root: string, paths: string[]) {
+  return paths.map(p => path.relative(root, p));
 }
