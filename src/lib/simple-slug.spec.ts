8411b3abc61d8b9f08ccf8666b71a09eb5749be2
Riceball LEE <snowyu.lee@gmail.com>
2020-07-12 16:28:48 +0800

    fix: more special chars filter for filename

--- src/lib/simple-slug.spec.ts
+++ src/lib/simple-slug.spec.ts
@@ -2,7 +2,7 @@ import { simpleSlug } from './simple-slug';
 
 describe('simpleSlug', () => {
   it('should filter multi whitespace or dot...', () => {
-    let result = simpleSlug('  test  ..  it....?md.');
+    let result = simpleSlug('  test --–-- ..  it....?md.');
     expect(result).toStrictEqual('test-it.md');
     result = simpleSlug('???--!!  test  ..  it....md???  ---++__!!');
     expect(result).toStrictEqual('test-it.md');
--- src/lib/simple-slug.ts
+++ src/lib/simple-slug.ts
@@ -6,8 +6,8 @@
  *  trimDots('hi test...md.') to 'hi-test.md'
  */
 export function simpleSlug(s: string) {
-  s = s.replace(/[ ?:%!]/g, '-');
-  s = s.replace(/([. \-_+])[. \-_+]+/g, '$1');
-  s = s.replace(/(^[. \-_+]+)|[. \-_+]+$/g, '');
+  s = s.replace(/[ ?:%!！：\\–？=+_]/g, '-');
+  s = s.replace(/([.\-])[.\-]+/g, '$1');
+  s = s.replace(/(^[.\-]+)|[.\-]+$/g, '');
   return s;
 }
